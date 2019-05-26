3989504671a8d9ac09bf14c02bee695b28276c82
신재욱(Shin Jaeuk) <luke7864@naver.com>
2019-05-26 16:12:59 +0900

    Add files via upload

---
+++ discord_server.py
@@ -0,0 +1,107 @@
+import discord
+import asyncio
+import requests
+
+client=discord.Client()
+client_secret_key=input("Input Client Secret Key: \n")
+server_host=input("서버의 호스트 주소를 알려주세요(IP주소나 도메인(http나 https 입력금지)만 입력해주세요: \n")
+server_port=input("호스트의 SSH 서비스가 구동되는 포트를 알려주세요: \n")
+server_key=input("서버에서 작동하고 있는 api의 비밀 키를 입력해주세요: \n")
+print("\n\n_______________ST4RT_____________________")
+ssh_info=server_host+' : '+server_port
+api_url='http://'+server_host+':'+'7864/api?key='+server_key
+
+@client.event
+async def on_ready():
+    print("Logged in as : "+str(client.user.name)+", "+str(client.user.id))
+    print("-------------------")
+
+try:
+    @client.event
+    async def on_message(message):
+        if message.content.startswith('!server'):
+            await client.send_message(message.channel, '수행할 동작을 알려주세요.(update,install,check,adduser,info)\n'
+                                                       '사용법이 궁금하신 분은 info를 입력해주세요.')
+            msg = await client.wait_for_message(timeout=15.0, author=message.author)
+
+
+            if msg is None:
+                await client.send_message(message.channel, '제한시간 15초가 지났습니다. 다시 시도해주세요. 날 기다리게하다니 미쳤습니까 휴먼?')
+            else:
+                msg=str(msg.content)
+                if msg=='info':
+                    await client.send_message(message.channel,'서버정보:\n'
+                                                            'SSH 연결 접속 정보: '+ssh_info+'\n\n'
+                                                            '사용법 안내:\n'
+                                                            '먼저 봇을 작동시키시려면 !server 라고 입력해주세요.'
+                                                            '현재는 ubuntu 기반 운영체제만 작동됩니다..\n\n'
+                                                            '명령어 안내:\n'
+                                                            'apt-get update를 하시려면 명령어는 update\n'
+                                                            'apt-get install을 하시려면 명령어는 install\n'
+                                                            '현재 설치된 apt 패키지가 있나 확인하려면 명령어는 check\n'
+                                                            'adduser을 하시려면 명령어는 adduser(패스워드는 teamkornewbie이니 ssh접속후 변경해주세요.\n'
+                                                            '그외 사용법이나 정보를 보려면 명령어는 info\n'
+                                                            '\n 더 자세한 정보는 http://contorl.jaeuk.xyz를 참고해주세요.'
+                                                            '\n해당봇과 해당서버 api의 개발자는 신재욱~!')
+                elif msg=='update':
+                    url=api_url+'&action=update'
+                    send=requests.get(url)
+                    response=str(send.text)
+                    await client.send_message(message.channel, response)
+
+                elif msg=='install':
+                    await client.send_message(message.channel, '설치할 패키지 이름을 알려주세요.')
+                    install = await client.wait_for_message(timeout=15.0, author=message.author)
+                    if install is None:
+                        await client.send_message(message.channel, '제한시간 15초가 지났습니다. 다시 시도해주세요. 날 기다리게 하다니 미쳤습니까 휴먼?')
+                    else:
+                        install=str(install.content)
+                        url=api_url+'&action=install&install='+install
+                        send=requests.get(url)
+                        response=str(send.text)
+                        await client.send_message(message.channel, response)
+
+                elif msg=='check':
+                    await client.send_message(message.channel, '확인할 패키지 명을 알려주세요')
+                    check = await client.wait_for_message(timeout=15.0, author=message.author)
+                    if check is None:
+                        await client.send_message(message.channel, '제한시간 15초가 지났습니다. 다시 시도해주세요. 날 기다리게 하다니 미쳤습니까 휴먼?')
+                    else:
+                        check=str(check.content)
+                        url=api_url+'&action=check&check='+check
+                        send=requests.get(url)
+                        response=str(send.text)
+                        await client.send_message(message.channel, response)
+
+                elif msg=='adduser':
+                    await client.send_message(message.channel, '생성할 유저 이름을 알려주세요.')
+                    adduser = await client.wait_for_message(timeout=15.0, author=message.author)
+                    if adduser is None:
+                        await client.send_message(message.channel, '제한시간 15초가 지났습니다. 다시 시도해주세요. 날 기다리게 하다니 미쳤습니까 휴먼?')
+                    else:
+                        adduser=str(adduser.content)
+                        url = api_url+'&action=adduser&adduser=' + adduser
+                        send = requests.get(url)
+                        response = str(send.text)
+                        await client.send_message(message.channel, response)
+
+        elif message.content.startswith('!'):
+            await client.send_message(message.channel, '서버정보:\n'
+                                                        'SSH 연결 접속 정보: ' + ssh_info + '\n\n'
+                                                        '사용법 안내:\n'
+                                                        '먼저 봇을 작동시키시려면 !server 라고 입력해주세요.'
+                                                         '현재는 ubuntu 기반 운영체제만 작동됩니다..\n\n'
+                                                        '명령어 안내:\n'
+                                                        'apt-get update를 하시려면 명령어는 update\n'
+                                                        'apt-get install을 하시려면 명령어는 install\n'
+                                                        '현재 설치된 apt 패키지가 있나 확인하려면 명령어는 check\n'
+                                                        'adduser을 하시려면 명령어는 adduser(패스워드는 teamkornewbie이니 ssh접속후 변경해주세요.\n'
+                                                        '그외 사용법이나 정보를 보려면 명령어는 info\n'
+                                                        '\n 더 자세한 정보는 http://contorl.jaeuk.xyz를 참고해주세요.'
+                                                        '\n해당봇과 해당서버 api의 개발자는 신재욱~!')
+
+except:
+    @client.event
+    async def on_message(message):
+        await client.send_message(message.channel, 'Error가 발생했습니다. 휴쳤습니까 미먼?')
+client.run(client_secret_key)
---
+++ web_api.py
@@ -0,0 +1,75 @@
+from flask import *
+import subprocess
+from string import ascii_lowercase,ascii_uppercase
+
+listallow=list(ascii_lowercase)
+listallow+=list(ascii_uppercase)
+listallow+=['_','-']
+listallow+=['1','2','3','4','5','6','7','8','9','0']
+
+make_key=input("api 통신에 사용할 비밀 KEY를 입력해주세요: \n")
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return "home"
+
+
+@app.route("/api", methods=['GET'])
+def api():
+    action=request.args.get('action')
+    key=request.args.get('key')
+
+    if key==make_key:
+        if action=='update':
+            out=subprocess.check_output(['apt-get update'], shell=True)
+            return out
+
+        elif action=='install':
+            install=request.args.get('install')
+            if install=="" or install==" ":
+                return "Error: NO install param"
+            else:
+                for i in range(len(install)):
+                    if list(install)[i] not in listallow:
+                        return "NO Hack~~!, IP has been collected"
+                out = subprocess.check_output(['apt-get -y install '+install], shell=True)
+                return out
+
+        elif action=='check':
+            check = request.args.get('check')
+            if check == "" or check == " ":
+                return "Error: NO install param"
+            else:
+                for i in range(len(check)):
+                    if list(check)[i] not in listallow:
+                        return "NO HACK~~!, IP has been collected"
+                try:
+                    out = subprocess.check_output(['dpkg -l | grep '+check], shell=True)
+                    return out
+                except:
+                    return "해당 패키지가 존재하지 않습니다."
+
+        elif action=='adduser':
+            adduser = request.args.get('adduser')
+            passwd="teamkornewbie"
+            if adduser == "" or adduser == " ":
+                return "Error: NO install param"
+            else:
+                for i in range(len(adduser)):
+                    if list(adduser)[i] not in listallow:
+                        return "NO Hack~~!, IP has been collected"
+                exec = subprocess.check_output(['useradd '+adduser+' -m -s /bin/bash'], shell=True)
+                chpass=subprocess.check_output(['echo "'+adduser+':'+passwd+'" | chpasswd'], shell=True)
+                out="유저명 "+adduser+"을 패스워드 teamkornewbie로 생성완료"
+                return out
+        else:
+            "ERROR: Exception ERROR"
+
+    else:
+        return "403 Permission Error"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7864)
